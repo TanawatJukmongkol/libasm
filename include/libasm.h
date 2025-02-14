28fb647749067db941b54007256fe88666376ba0
Tanawat Jukmongkol <tjukmong@z3t3c4.42bangkok.com>
2025-02-15 00:44:11 +0700

    code

---
+++ include/libasm.h
@@ -0,0 +1,9 @@
+#ifndef	LIBASM_H
+# define LIBASM_H
+
+#include <stdlib.h>
+#include <errno.h>
+
+extern ssize_t ft_write(int fd, const void *buf, size_t count);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include "libasm.h"
+
+int main()
+{
+	ft_write(1, "abc\n" , 4);
+	return 0;
+}
