7736c8a2be2146bbe769580907da473d6e73006a
TanawatJukmongkol <tanawat.jukmon@gmail.com>
2025-02-19 05:20:02 +0700

    added header

---
+++ tester/include/tests.h
@@ -0,0 +1,15 @@
+#ifndef	TESTS_H
+# define TESTS_H
+
+#include "utils.h"
+#include <fcntl.h>
+#include <stdint.h>
+
+int	tester_read();
+int	tester_write();
+int	tester_strlen();
+int	tester_strcmp();
+int	tester_strcpy();
+int	tester_strdup();
+
+#endif
